bd6212399c4cf681858b296990718734ea36a9fa
Vivian Deng <77829789+vydeng@users.noreply.github.com>
2023-03-03 12:48:38 -0500

    more progress, think i got the right url

--- cli.js
+++ cli.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
+import minimist from 'minimist'
+import moment from 'moment-timezone'
 
-function show_help() {
+var args = minimist(process.argv.slice(2));
+
+
+
+
+if (args.h) {
     console.log(`Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
 
         -h\t\tShow this help message and exit.\n"
@@ -13,3 +20,19 @@ function show_help() {
 }
 
 const timezone = moment.tz.guess()
+
+// Make a request
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=America%2FNew_York');
+
+// Get the data from the request
+const data = await response.json();
+
+const days = args.d 
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
