26b67970cd978c13a44d69e6cf8f40220fcfe040
Vivian Deng <77829789+vydeng@users.noreply.github.com>
2023-03-02 20:07:09 -0500

    errors went away ?

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 function show_help() {
-    printf -- "Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n"
-    printf -- ;"\n"
-    printf -- ;"  -h\t\tShow this help message and exit.\n"
-    printf -- ;"  -n, -s\tLatitude: N positive; S negative.\n"
-    printf -- ;"  -e, -w\tLongitude: E positive; W negative.\n"
-    printf -- ;"  -z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n"
-    printf -- ;"  -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
-    printf -- ;"  -j\t\tEcho pretty JSON from open-meteo API and exit.\n"
+    console.log(`Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+
+        -h\t\tShow this help message and exit.\n"
+        -n, -s\tLatitude: N positive; S negative.\n"
+        -e, -w\tLongitude: E positive; W negative.\n"
+        -z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n"
+        -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
+        -j\t\tEcho pretty JSON from open-meteo API and exit.\n"`)
     exit(0)
 }
 
