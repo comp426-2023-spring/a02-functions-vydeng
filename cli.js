280bb2caeb416721ac932236ff2147259a62434f
Vivian Deng <77829789+vydeng@users.noreply.github.com>
2023-03-05 13:33:04 -0500

    fixed formatting of help message

--- cli.js
+++ cli.js
@@ -9,13 +9,12 @@ var args = minimist(process.argv.slice(2));
 
 if (args.h) {
     console.log(`Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-
-        -h\tShow this help message and exit.\n"
-        -n, -s\tLatitude: N positive; S negative.\n"
-        -e, -w\tLongitude: E positive; W negative.\n"
-        -z\tTime zone: uses tz.guess() from moment-timezone by default.\n"
-        -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
-        -j\tEcho pretty JSON from open-meteo API and exit.\n"`);
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit`);
     process.exit(0);
 }
 
