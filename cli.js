c5f90fc70b7173d1381c6420ddfd0392049a9124
Vivian Deng <77829789+vydeng@users.noreply.github.com>
2023-03-06 13:09:22 -0500

    fixed help

--- cli.js
+++ cli.js
@@ -14,20 +14,19 @@ if (args.h) {
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit`);
+    -j            Echo pretty JSON from open-meteo API and exit.`);
 	process.exit(0);
 }
 
 const timezone = args.z ?? moment.tz.guess();
-let longitude = 0;
-let latitude = 0;
 
-if (args.e || args.w) {
-longitude = args.e || args.w * -1;
-}
-if (args.n || args.s) {
-  latitude = args.n || args.s * -1;
-}
+const longitude = args.e || args.w * -1;
+const latitude = args.n || args.s * -1;
+
+// if (!longitude || !latitude) {
+//   console.log("Latitude must be in range");
+//   process.exit(0);
+// }
 
 // Make a request
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
